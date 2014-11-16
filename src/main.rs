9cb1515923672265bde1491825eb32de2a2a303a
Ian Cullinan <cibyr@cibyr.com>
2014-11-16 12:06:32 -0800

    get friendly name

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,14 @@ fn get_location(response: &[u8]) -> Option<&str> {
     }
 }
 
+fn get_friendly_name(body: &str) -> Option<&str> {
+    let re = regex!(r"<friendlyName>(.+?)</friendlyName>");
+    match re.captures(body) {
+        Some(cap) => Some(cap.at(1)),
+        None => None
+    }
+}
+
 fn main() {
     let any_addr = SocketAddr { ip: Ipv4Addr(0, 0, 0, 0), port: 0 };
     let dst_addr = SocketAddr { ip: Ipv4Addr(239, 255, 255, 250), port: 1900 };
@@ -64,8 +72,9 @@ fn main() {
                     _ => panic!("No app url from {}", location)
                 };
                 println!("App url: {}", from_utf8(app_url.as_slice()).unwrap());
-
-
+                let body = res.read_to_string().ok().expect("read body");
+                let friendly_name = get_friendly_name(body.as_slice());
+                println!("Friendly name: {}", friendly_name);
             }
             _ => break
         }
