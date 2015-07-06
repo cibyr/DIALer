b29f5129226eb36f4ad862609378350b8a8c667f
Ian Cullinan <cibyr@cibyr.com>
2015-07-05 17:57:08 -0700

    Changes required to build on today's nightly - using the shiny new set_read_timeout()

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+#![feature(duration)]
+#![feature(ip_addr)]
+#![feature(slice_extras)]
 #![feature(plugin)]
+#![feature(socket_timeout)]
+
 #![plugin(regex_macros)]
 
 #[macro_use] extern crate log;
@@ -14,23 +19,24 @@ use hyper::client::Request;
 use hyper::header::{ContentLength, ContentType};
 use hyper::method::Method;
 use hyper::mime::Mime;
-use hyper::mime::TopLevel::Text;
 use hyper::mime::SubLevel::Plain;
+use hyper::mime::TopLevel::Text;
 use hyper::Url;
+use std::env;
 use std::error::Error;
-use std::error::FromError;
 use std::fmt;
-use std::old_io::net::ip::{Ipv4Addr, SocketAddr};
-use std::old_io::net::udp::UdpSocket;
-use std::os;
+use std::io::Read;
+use std::io::Write;
+use std::net::UdpSocket;
+use std::net::Ipv4Addr;
 use std::str::from_utf8;
-
+use std::time::Duration;
 
 #[derive(Debug)]
 enum DialError {
     DialProtocolError,
-    HttpError(hyper::HttpError),
-    IoError(std::old_io::IoError),
+    HttpError(hyper::error::Error),
+    IoError(std::io::Error),
     UrlParseError(url::ParseError),
 }
 type DialResult<T> = Result<T, DialError>;
@@ -63,20 +69,20 @@ impl fmt::Display for DialError {
     }
 }
 
-impl FromError<std::old_io::IoError> for DialError {
-    fn from_error(err: std::old_io::IoError) -> DialError {
+impl std::convert::From<std::io::Error> for DialError {
+    fn from(err: std::io::Error) -> DialError {
         IoError(err)
     }
 }
 
-impl FromError<hyper::HttpError> for DialError {
-    fn from_error(err: hyper::HttpError) -> DialError {
+impl std::convert::From<hyper::error::Error> for DialError {
+    fn from(err: hyper::error::Error) -> DialError {
         HttpError(err)
     }
 }
 
-impl FromError<url::ParseError> for DialError {
-    fn from_error(err: url::ParseError) -> DialError {
+impl std::convert::From<url::ParseError> for DialError {
+    fn from(err: url::ParseError) -> DialError {
         UrlParseError(err)
     }
 }
@@ -88,7 +94,7 @@ macro_rules! try_log(
         Ok(e) => e,
         Err(e) => {
             error!($($arg)*, e);
-            return Err(::std::error::FromError::from_error(e));
+            return Err(::std::convert::From::from(e));
         },
     })
 );
@@ -131,8 +137,8 @@ fn get_friendly_name(body: &str) -> Option<&str> {
 
 
 fn discover_dial_locations() -> DialResult<Vec<String>> {
-    let any_addr = SocketAddr { ip: Ipv4Addr(0, 0, 0, 0), port: 0 };
-    let dst_addr = SocketAddr { ip: Ipv4Addr(239, 255, 255, 250), port: 1900 };
+    let any_addr = (Ipv4Addr::new(0, 0, 0, 0), 0);
+    let dst_addr = (Ipv4Addr::new(239, 255, 255, 250), 1900);
     let ssdp_msearch =
         "M-SEARCH * HTTP/1.1\r\n\
         HOST: 239.255.255.250:1900\r\n\
@@ -140,17 +146,17 @@ fn discover_dial_locations() -> DialResult<Vec<String>> {
         MX: 2\r\n\
         ST: urn:dial-multiscreen-org:service:dial:1\r\n\r\n";
 
-    let mut socket = try_log!(UdpSocket::bind(any_addr), "bind: {}");
-    debug!("Sending to {}:\n{}", dst_addr, ssdp_msearch);
+    let socket = try_log!(UdpSocket::bind(any_addr), "bind: {}");
+    debug!("Sending to {:?}:\n{}", dst_addr, ssdp_msearch);
     try_log!(socket.send_to(ssdp_msearch.as_bytes(), dst_addr), "send_to: {}");
-    socket.set_timeout(Some(3000));  // 3 second timeout
+    try_log!(socket.set_read_timeout(Some(Duration::from_secs(3))), "set_read_timeout: {}");
 
     let mut result = Vec::new();
     loop {
         let buf: &mut[u8] = &mut [0; 4096];
         match socket.recv_from(buf) {
             Ok((len, addr)) => {
-                debug!("Received from {}:\n{:?}", addr.ip, from_utf8(buf.slice(0, len)));
+                debug!("Received from {}:\n{:?}", addr.ip(), from_utf8(&buf[0..len]));
                 match get_location(&buf[0 .. len]) {
                     Some(location) => result.push(location.to_string()),
                     None => ()
@@ -177,20 +183,26 @@ impl DialServer {
         let mut res = try_log!(try_log!(req
                 .start(), "Error writing headers: {}")
                 .send(), "Error reading response headers: {}");
-        let body = match res.read_to_string() {
-            Ok(body) => Some(body),
+        let mut buf = String::new();
+        let body = match res.read_to_string(&mut buf) {
+            Ok(_) => Some(buf),
             Err(e) => {
                 error!("Couldn't read body: {}", e);
                 None
             },
         };
         let rest_url = match res.headers.get_raw("Application-URL") {
-            Some([ref rest_url]) => match from_utf8(rest_url.as_slice()) {
-                Ok(rest_url) => rest_url,
-                Err(_) => {
-                    error!("invalid app url: {:?}", rest_url);
-                    return Err(DialProtocolError);
-                },
+            Some(rest_urls) => if rest_urls.len() == 1 {
+                match from_utf8(&rest_urls[0]) {
+                    Ok(rest_url) => rest_url,
+                    Err(_) => {
+                        error!("invalid app url: {:?}", rest_urls[0]);
+                        return Err(DialProtocolError);
+                    },
+                }
+            } else {
+                error!("Multiple app URLs from {}", location);
+                return Err(DialProtocolError);
             },
             _ => {
                 error!("No app url from {}", location);
@@ -198,7 +210,7 @@ impl DialServer {
             },
         };
         let friendly_name = match body {
-            Some(body) => match get_friendly_name(body.as_slice()) {
+            Some(body) => match get_friendly_name(&body) {
                 Some(name) => Some(name.to_string()),
                 None => None,
             },
@@ -227,7 +239,7 @@ impl DialServer {
 
     fn has_app(&self, app_name: &str) -> bool {
         let url_string = self.get_app_url(app_name);
-        let url = try_log_return!(Url::parse(url_string.as_slice()), false, "invalid url: {}");
+        let url = try_log_return!(Url::parse(&url_string), false, "invalid url: {}");
         let req = try_log_return!(Request::new(Method::Get, url), false, "Failed to connect to {}: {}", url_string);
         let started = try_log_return!(req.start(), false, "Error writing headers: {}");
         let res = try_log_return!(started.send(), false, "Error reading response headers: {}");
@@ -237,7 +249,7 @@ impl DialServer {
     fn launch_app(&self, app_name: &str, payload: Option<&str>) -> DialResult<()> {
         info!("Launching {} with payload {:?}", app_name, payload);
         let url_string = self.get_app_url(app_name);
-        let url = try_log!(Url::parse(url_string.as_slice()), "invalid url: {}");
+        let url = try_log!(Url::parse(&url_string), "invalid url: {}");
         let mut req = try_log!(Request::new(Method::Post, url), "Failed to connect to {}: {}", url_string);
         match payload {
             Some(payload) => req.headers_mut().set(ContentLength(payload.len() as u64)),
@@ -260,14 +272,14 @@ impl DialServer {
 
 fn print_usage(program: &str, opts: Options) {
     let brief = format!("Usage: {} [options] [payload]", program);
-    print!("{}", opts.usage(brief.as_slice()));
+    print!("{}", opts.usage(&brief));
 }
 
 
 fn main() {
     env_logger::init().unwrap();
 
-    let args: Vec<String> = os::args();
+    let args: Vec<String> = env::args().collect();
     let ref program = args[0];
 
     let mut options = Options::new();
@@ -278,20 +290,21 @@ fn main() {
         Err(f) => { panic!(f.to_string()) }
     };
     if matches.opt_present("h") {
-        print_usage(program.as_slice(), options);
+        print_usage(&program, options);
         return;
     }
 
     let app = matches.opt_str("a");
     let payload = if !matches.free.is_empty() {
-        Some(matches.free[0].as_slice())
+        let payload: &str = &matches.free[0];
+        Some(payload)
     } else {
         None
     };
 
     let locations = discover_dial_locations().ok().expect("discovery failed");
     let servers = locations.iter().filter_map(|loc| {
-        match DialServer::new(loc.as_slice()) {
+        match DialServer::new(&loc) {
             Ok(server) => Some(server),
             Err(e)     => {
                 error!("Couldn't understand DIAL server at location {}: {}", loc, e);
@@ -302,10 +315,10 @@ fn main() {
 
     match app {
         Some(app) => {
-            let servers: Vec<DialServer> = servers.filter(|s| s.has_app(app.as_slice())).collect();
+            let servers: Vec<DialServer> = servers.filter(|s| s.has_app(&app)).collect();
             match servers.len() {
                 0 => panic!("Found no DIAL servers with application {:?}", app),
-                1 => servers[0].launch_app(app.as_slice(), payload).ok().expect("failure to launch"),
+                1 => servers[0].launch_app(&app, payload).ok().expect("failure to launch"),
                 _ => unimplemented!(), // TODO: allow a choice betwwen multiple servers
             }
         },
